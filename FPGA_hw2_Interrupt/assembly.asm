c641bebfa360bacebeefc778c1d72eb799e0a596
A.Talha Timur <93510585+ATalhaTimur@users.noreply.github.com>
2024-01-13 20:18:24 +0300

    asm change

--- FPGA_hw2_Interrupt/assembly.asm
+++ FPGA_hw2_Interrupt/assembly.asm
@@ -5,22 +5,22 @@
         ldi 1 isr
         st  0 1	
 
-main_l  sti
+main    sti
         ldi 2 0x901
         ldi 3 0x0001
         ld 2 2
         sub 2 2 3
         cli	
-        jz left_b	  	
-        jmp to_dec
+        jz button_l	  	
+        jmp cvrt_dec
 
-left_b  
+button_l  
         ldi 3 0x900	
         ld  3 3
         inc 3
         ldi 4 0x900
         st 4 3
-        jmp main_l
+        jmp main
 
 isr     cli
         ldi 3 0x900
@@ -33,37 +33,37 @@ isr     cli
 
 display ldi 1 0xb00
         st 1 2
-        jmp main_l
+        jmp main
 
-to_dec  ldi 1 0x900
+cvrt_dec        ldi 1 0x900
 	ld 1 1
         ldi 4 0x0010
         ldi 2 0x0000
-alt_loop    ldi 3 0x000f
+loop    ldi 3 0x000f
         and 3 3 2
         ldi 5 0x0004
         sub 3 5 3
         ldi 5 0x8000
         and 3 3 5
-        jz check2
+        jz ctrl2
         jmp add3_1
-check2  ldi 3 0x00f0
+ctrl2  ldi 3 0x00f0
         and 3 3 2
         ldi 5 0x0040
         sub 3 5 3
         ldi 5 0x8000
         and 3 3 5
-        jz check3
+        jz ctrl3
         jmp add3_2
-check3  ldi 3 0x0f00
+ctrl3  ldi 3 0x0f00
         and 3 3 2
         ldi 5 0x0400
         sub 3 5 3
         ldi 5 0x8000
         and 3 3 5
-        jz check4
+        jz ctrl4
         jmp add3_3
-check4  ldi 3 0xf000
+ctrl4  ldi 3 0xf000
         and 3 3 2
         ldi 5 0x4000
         sub 3 5 3
@@ -80,16 +80,16 @@ phase2  add 2 2 2
 msb0    add 1 1 1
         dec 4
         jz display
-        jmp alt_loop
+        jmp loop
 add3_1  ldi 5 0x0003
         add 2 2 5
-        jmp check2
+        jmp ctrl2
 add3_2  ldi 5 0x0030
         add 2 2 5
-        jmp check3
+        jmp ctrl3
 add3_3  ldi 5 0x0300
         add 2 2 5
-        jmp check4
+        jmp ctrl4
 add3_4  ldi 5 0x3000
         add 2 2 5
         jmp phase2
